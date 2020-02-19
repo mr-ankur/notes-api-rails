25b3fe9ddad7629dd45707bed923b0b64c107f2f
Mohammad Farhan <farhan@akaruilabs.com>
2020-02-19 12:19:23 +0530

    code cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+Swagger::Docs::Config.base_api_controller = ActionController::API
+include Swagger::Docs::ImpotentMethods
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, :defaults => {:format => :json} do
